98e06d0a7d63859af9043ea400284c3bb066873b
Adam Blakey <adam@blakey.family>
2022-06-12 21:33:27 +0100

    +

--- includes/header.php
+++ includes/header.php
@@ -129,7 +129,7 @@
                   <?php
                 }
               ?>
-              <a href="./logout.php?redirect_page=<?=urlencode($_SERVER["REQUEST_URI"]);?>" class="dropdown-item">Logout</a>
+              <a href="<?=$config['base_url'];?>/logout.php?redirect_page=<?=urlencode($_SERVER["REQUEST_URI"]);?>" class="dropdown-item">Logout</a>
             </div>
           </div>
         <?php
@@ -137,7 +137,7 @@
         else
         {
         ?>
-            <a href="./login.php?redirect_page=<?=urlencode($_SERVER["REQUEST_URI"]);?>" class="nav-link d-flex lh-1 text-reset p-0" aria-label="Login">
+            <a href="<?=$config['base_url'];?>/login.php?redirect_page=<?=urlencode($_SERVER["REQUEST_URI"]);?>" class="nav-link d-flex lh-1 text-reset p-0" aria-label="Login">
               <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-login" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                 <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
