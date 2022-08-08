df7069cfe1021e564ba0eab127d6cd23962c5f95
Adam Blakey <adam@blakey.family>
2022-08-08 21:08:49 +0100

    gitflow-hotfix-stash: _featured_background
    Fixed featured column background colour.

--- includes/head.php
+++ includes/head.php
@@ -2,12 +2,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
 <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
 <!-- CSS files -->
-<link href="<?=$config['base_url'];?>/dist/css/tabler.min.css?v=1.2" rel="stylesheet"/>
+<!-- <link href="<?=$config['base_url'];?>/dist/css/tabler.min.css?v=1.2" rel="stylesheet"/>
 <link href="<?=$config['base_url'];?>/dist/css/tabler-flags.min.css" rel="stylesheet"/>
 <link href="<?=$config['base_url'];?>/dist/css/tabler-payments.min.css" rel="stylesheet"/>
 <link href="<?=$config['base_url'];?>/dist/css/tabler-vendors.min.css" rel="stylesheet"/>
-<link href="<?=$config['base_url'];?>/dist/css/demo.min.css" rel="stylesheet"/>
+<link href="<?=$config['base_url'];?>/dist/css/demo.min.css" rel="stylesheet"/> -->
 
 <!--<script src="https://unpkg.com/@tabler/core@1.0.0-beta10/dist/js/tabler.min.js"></script>-->
-<link href="<?=$config['base_url'];?>/dist/css/pollererer.min.css?v=1.1" rel="stylesheet"/>
-<!--<link rel="stylesheet" href="https://unpkg.com/@tabler/core@1.0.0-beta10/dist/css/tabler.min.css">-->
+<link href="<?=$config['base_url'];?>/dist/css/pollererer.min.css?v=1.2" rel="stylesheet"/>
+<link rel="stylesheet" href="https://unpkg.com/@tabler/core@1.0.0-beta10/dist/css/tabler.min.css">
