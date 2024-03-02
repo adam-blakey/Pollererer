b442bf971ff78335a78208fa984edfa4dea583a2
Adam Blakey <adam@blakey.family>
2024-03-02 16:27:12 +0000

    ~ Fixed issue with service worker for PWA. - Removed the poll ribbons. + Added term and ensemble images.

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     addResourcesToCache([
-      '/',
-      '/dist/pollererer.css'
+      '/'
     ])
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(async function() {
-     try{
-       var res = await fetch(event.request);
-       var cache = await caches.open('cache');
-       cache.put(event.request.url, res.clone());
-       return res;
-     }
-     catch(error){
-       return caches.match(event.request);
-      }
-    }());
+    if(event.request.url.startsWith('chrome-extension')){
+        //skip request
+    }
+    else {
+        event.respondWith(async function() {
+            try{
+                var res = await fetch(event.request);
+                var cache = await caches.open('cache');
+                cache.put(event.request.url, res.clone());
+                return res;
+            }
+            catch(error){
+                return caches.match(event.request);
+            }
+        }());   
+    }
 });
